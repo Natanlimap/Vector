1dfa3573e1e08409b58a4f912e27d4359928ada6
Natanlimap <natanlimap@gmail.com>
2019-10-11 23:25:37 -0300

    final

--- src/app2.cpp
+++ src/app2.cpp
@@ -8,10 +8,10 @@
 
 int main () {
     sc::vector<int> C{5, 6};
-    sc::vector<int> D{1,2, 3,4};
+    sc::vector<int> D{1,2, 3,4, 5};
     sc::vector<int> B;
      D.print();
-    D.erase(D.begin()+1, D.begin()+1);
+    D.insert(D.begin(),C.begin(), C.begin()+1);
     D.print();
     return 0;
 }
